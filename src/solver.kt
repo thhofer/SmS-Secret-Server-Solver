9086534cd85b7773950586ae7f16ccb5454dc811
Thomas Hofer <thomas.geek.hofer@gmail.com>
2017-11-18 13:35:26 +0100

    Style fixes

--- src/solver.kt
+++ src/solver.kt
@@ -3,8 +3,8 @@ import java.math.BigInteger
 val p = BigInteger("14219462995139870823732990991847116988782830807352488252401693038616204860083820490505711585808733926271164036927426970740721056798703931112968394409581", 10)
 val g = BigInteger("13281265858694166072477793650892572448879887611901579408464846556561213586303026512968250994625746699137042521035053480634512936761634852301612870164047", 10)
 val keyLength = 32
-val TWO = BigInteger.valueOf(2L)
-val ths = p.subtract(BigInteger.ONE).divide(TWO)
+val two: BigInteger = BigInteger.valueOf(2L)
+val ths: BigInteger = p.subtract(BigInteger.ONE).divide(two)
 
 fun main (args: Array<String>) {
     println("Getting started")
@@ -29,7 +29,7 @@ fun computeKey(startingState: BigInteger): BigInteger {
     for (i in 0..(keyLength*8-1)) {
         x = g.modPow(x, p)
         if (x > ths) {
-            ret = ret.add(TWO.pow(i))
+            ret = ret.add(two.pow(i))
         }
     }
     return ret
