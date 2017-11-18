ce1d10411a17c9ba5eef8921fc7b975efb82fd5a
Thomas Hofer <thomas.geek.hofer@gmail.com>
2017-11-18 13:07:52 +0100

    Compute members of group

---
+++ src/CyclicGroup.kt
@@ -0,0 +1,16 @@
+import java.math.BigInteger
+
+class CyclicGroup(val p: BigInteger, val g: BigInteger) {
+    fun members(): List<BigInteger> {
+        val valuesFound: MutableSet<BigInteger> = HashSet()
+        var member: BigInteger
+
+        member = g
+        while (!valuesFound.contains(member)) {
+            valuesFound.add(member)
+            member = member.multiply(g).mod(p)
+        }
+
+        return valuesFound.toList()
+    }
+}
